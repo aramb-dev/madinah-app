7ebf45be5566e0d042397e56299f3840f958ee26
Abdur Rahman Bilal <aramb@aramservices.com>
2025-06-17 21:34:23 -0400

    fix(layout): remove duplicate FontAwesome import that was causing SyntaxError during iOS bundling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import { useColorScheme } from '@/components/useColorScheme';
 
